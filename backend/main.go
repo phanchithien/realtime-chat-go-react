e79fc91b3bb87b3c78f1d9f964a1c1c59a941d15
Phan Chi Thien <phanchithien456@gmail.com>
2023-06-27 16:48:50 +0700

    first commit

---
+++ backend/main.go
@@ -0,0 +1,39 @@
+package main 
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/phanchithien/realtime-chat-go-react/pkg/websocket"
+)
+
+func serveWs(pool *websocket.Pool, w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Websocket Endpoint Hit")
+	conn, err := websocket.Upgrade(w, r)
+	if err != nil {
+		fmt.Println(w, "%+V\n", err)
+	}
+
+	client := &websocket.Client{
+		Conn: conn,
+		Pool: pool,
+	}
+
+	pool.Register <- client
+	client.Read()
+}
+
+func setupRoutes() {
+	pool := websocket.NewPool()
+	go pool.Start()
+
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		serveWs(pool, w, r)
+	})
+}
+
+func main() {
+	fmt.Println("Distributed Chat App v0.01")
+	setupRoutes()
+	http.ListenAndServe(":8080", nil)
+}
